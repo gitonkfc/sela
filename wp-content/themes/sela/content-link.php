d2598923efa45c3d30259f2f2e984c2f549ed265
gitonkfc <gitongit@tuta.io>
2018-09-20 23:25:48 +0700

    change prefix, add share social button, revisi related post

---
+++ wp-content/themes/sela/content-link.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package poly
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php poly_post_thumbnail(); ?>
+
+	<header class="entry-header">
+		<?php if ( is_single() ) : ?>
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php else : ?>
+			<?php the_title( '<h1 class="entry-title"><a href=" ' . esc_url( poly_get_link_url() ). '" rel="bookmark">', '</a></h1>' ); ?>
+		<?php endif; // is_single() ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-body">
+		<div class="entry-meta">
+			<?php poly_entry_meta(); ?>
+		</div><!-- .entry-meta -->
+
+		<div class="entry-content">
+			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'poly' ) ); ?>
+			<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'poly' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
+
+		<?php if ( is_single() ) : ?>
+		<footer class="entry-meta">
+			<?php poly_footer_entry_meta(); ?>
+		</footer><!-- .entry-meta -->
+		<?php endif; ?>
+
+		<?php poly_author_bio(); ?>
+	</div><!-- .entry-body -->
+
+</article><!-- #post-## -->
